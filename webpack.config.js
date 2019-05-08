5a8979b12938b760b290c00d8d700746391e3769
Elena Rakhimova <elenara@kth.se>
2019-05-08 09:57:03 +0200

    Renamed css file in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 module.exports = {
   entry:
-    [ '@babel/polyfill', './public/js/app/app.jsx', './public/css/kursinfo-web.scss' ],
+    [ '@babel/polyfill', './public/js/app/app.jsx', './public/css/kursutveckling-admin.scss' ],
   target: 'node',
   output: {
     path: path.resolve(__dirname, 'dist'),
@@ -26,7 +26,7 @@ module.exports = {
     ]
   },
   plugins: [
-    new ExtractTextPlugin('css/kursinfo-web.css')
+    new ExtractTextPlugin('css/kursutveckling-admin.css')
   ],
   resolve: {
     extensions: ['.js', '.jsx']
