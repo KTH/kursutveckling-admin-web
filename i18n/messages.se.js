babd167c2bd07e0ec4aa8e07bd5070a391dd0354
Jeanette Skoog <jeanetteskoog@n169-p32.eduroam.kth.se>
2019-08-12 11:18:36 +0200

    test edge 2

--- i18n/messages.se.js
+++ i18n/messages.se.js
@@ -55,6 +55,11 @@ module.exports = {
     label_start_date: 'Startdatum',
 
     /* **** INTRO TEXT FOR ANALYSIS MENUE, EDIT, PREVIEW */
+    intro_analysis_menu: 'Börja med att välja termin och kursomgång för den kursanalys som ska publiceras (steg 1 av 3). I nästa steg (2 av 3) kommer kursdata för kursen att hämtas automatiskt från Ladok och Kopps för den termin och kursomgång som valts. Det finns sedan möjlighet att redigera viss kursdata samt ladda upp kursanalysen. I sista steget (3 av 3) ges möjlighet att först granska och sedan publicera tabellen med kursanalys och all kursdata på sidan Kursens utveckling och historik.',
+    intro_edit: 'I detta steg (2 av 3) så laddas kursanalys och kurs-PM upp, förändringar för vald kursomgång summeras samt en del av kursdata kontrolleras och vid behov justeras när inte stämmer. I nästa steg granskas all kursdata innan publicering.',
+    intro_preview: 'I detta steg (3 av 3) visas hur kursanalys och kursdata kommer att se ut på sidan Kursens utveckling och historik. Här finns möjlighet att gå tillbaka för att redigera ytterligare, spara som utkast eller publicera direkt.',
+
+    /* **** INTRO SELECT NEW, DRAFT, PUBLISHED */
     intro_new: 'Följande kurstillfällen saknar publicerad kursanalys/kursdata. Markera en eller flera som ingår i kursomgången: ',
     intro_draft: 'Följande kursomgångar har sparade utkast som ännu ej publicerats: ',
     intro_published: 'Följande kursomgångar har publicerad kursanalys och kursdata. Välj den kursomgång nedan som du vill du vill ändra och sedan publicera om: ',
