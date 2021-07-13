c989518af502dafe6dc130215040a07086448931
Elena Rakhimova <elenara@kth.se>
2021-07-13 18:30:33 +0200

    style: fix styling of i-icon buttons after kth-style update

--- public/js/app/components/InfoButton.jsx
+++ public/js/app/components/InfoButton.jsx
@@ -1,17 +1,23 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import { Button, UncontrolledPopover, PopoverHeader, PopoverBody } from 'reactstrap'
 
-
-const InfoButton =({ id, textObj, addClass }) => {
-    return (
-      <div className= {addClass ? addClass : ''}>
-        <Button id={id} type="button" className='btn-info-modal btn btn-secondary info-inline'/>
-        <UncontrolledPopover trigger="legacy" placement="auto"  target={id} >
-          <PopoverHeader>{textObj.header}</PopoverHeader>
-          <PopoverBody><p>{textObj.body}</p></PopoverBody>
-        </UncontrolledPopover>
-      </div>
-    )
-  }
+const InfoButton = ({ id, textObj, addClass }) => {
+  return (
+    <div className={addClass ? addClass : ''}>
+      <Button
+        id={id}
+        type="button"
+        className="btn-info-modal btn btn-secondary"
+        style={{ height: '1.3em', width: '1.3em' }}
+      />
+      <UncontrolledPopover trigger="legacy" placement="auto" target={id}>
+        <PopoverHeader>{textObj.header}</PopoverHeader>
+        <PopoverBody>
+          <p>{textObj.body}</p>
+        </PopoverBody>
+      </UncontrolledPopover>
+    </div>
+  )
+}
 
 export default InfoButton
--- public/js/app/components/InfoModal.jsx
+++ public/js/app/components/InfoModal.jsx
@@ -5,60 +5,59 @@ import { Modal, ModalBody, ModalHeader, ModalFooter, Button, Input } from 'react
 import CopyText from './CopyText'
 
 class InfoModal extends Component {
-  constructor (props) {
+  constructor(props) {
     super(props)
     this.state = {
-        modal: this.props.isOpen,
-        newEndDate: ''
-      }
+      modal: this.props.isOpen,
+      newEndDate: '',
+    }
     this.handleConfirm = this.handleConfirm.bind(this)
     this.handleDateChange = this.handleDateChange.bind(this)
   }
-  
-  handleConfirm(event){
+
+  handleConfirm(event) {
     event.preventDefault()
     const { type, handleConfirm } = this.props
     handleConfirm(this.props.id, true)
   }
-  
-  handleDateChange(event){
+
+  handleDateChange(event) {
     event.persist()
     this.state.newEndDate = event.target.value
   }
 
-
-  render () {
+  render() {
     const { fade, isOpen, toggle, className, type, infoText, id, url, copyHeader } = this.props
-    const fadeModal = (this.props.hasOwnProperty('fade') ? fade : true)
+    const fadeModal = this.props.hasOwnProperty('fade') ? fade : true
 
-      return (
-        <div>
-          {type === 'info'
-            ? <Button id={type} type="button"  onClick={toggle} className='btn-info-modal btn btn-secondary info-inline'/>
-            : ''
-          }
-          <Modal isOpen = {isOpen} toggle={toggle} className={className} fade={fadeModal} id={id}>
-            <ModalHeader toggle={toggle}>{infoText.header}</ModalHeader>
-            <ModalBody>
-              {type=== 'copy'
-                ? <CopyText textToCopy={url} header = {copyHeader} />
-                : <p dangerouslySetInnerHTML={{ __html:infoText.body}}/>
-              }
-            </ModalBody>
-            <ModalFooter>
-              <Button id={type} color='secondary' onClick={toggle}>{infoText.btnCancel}</Button>
-              {
-                infoText.btnConfirm
-                ?<Button color='secondary' onClick={this.handleConfirm}>{infoText.btnConfirm}</Button>
-                : ''
-              }
-            </ModalFooter>
-          </Modal>
-          </div>
-      )
-    }
+    return (
+      <div>
+        {type === 'info' && (
+          <Button id={type} type="button" onClick={toggle} className="btn-info-modal btn btn-secondary" />
+        )}
+        <Modal isOpen={isOpen} toggle={toggle} className={className} fade={fadeModal} id={id}>
+          <ModalHeader toggle={toggle}>{infoText.header}</ModalHeader>
+          <ModalBody>
+            {type === 'copy' ? (
+              <CopyText textToCopy={url} header={copyHeader} />
+            ) : (
+              <p dangerouslySetInnerHTML={{ __html: infoText.body }} />
+            )}
+          </ModalBody>
+          <ModalFooter>
+            <Button id={type} color="secondary" onClick={toggle}>
+              {infoText.btnCancel}
+            </Button>
+            {infoText.btnConfirm && (
+              <Button color="secondary" onClick={this.handleConfirm}>
+                {infoText.btnConfirm}
+              </Button>
+            )}
+          </ModalFooter>
+        </Modal>
+      </div>
+    )
   }
-
-  
+}
 
 export default InfoModal
