dbe963ef9214a66614c484196a3e53fee9c115cb
Karl Andin <karl.andin@drakryggen.com>
2024-06-10 10:44:36 +0200

    fix(KUI-1265): add close button to popovers

--- public/js/app/components/InfoButton.jsx
+++ public/js/app/components/InfoButton.jsx
@@ -1,16 +1,32 @@
 import React from 'react'
-import { UncontrolledPopover, PopoverHeader, PopoverBody } from 'reactstrap'
+import { Popover, PopoverHeader, PopoverBody } from 'reactstrap'
 
-const InfoButton = ({ id, textObj }) => (
-  <>
-    <button id={`button-to-activate-popup-${id}`} type="button" className="btn-info-modal" />
-    <UncontrolledPopover trigger="legacy" placement="auto" target={`button-to-activate-popup-${id}`}>
-      <PopoverHeader>{textObj.header}</PopoverHeader>
-      <PopoverBody>
-        <p>{textObj.body}</p>
-      </PopoverBody>
-    </UncontrolledPopover>
-  </>
-)
+const InfoButton = ({ id, textObj }) => {
+  const [isOpen, setIsOpen] = React.useState(false)
+
+  const toggle = () => setIsOpen(!isOpen)
+
+  return (
+    <>
+      <button id={`button-to-activate-popup-${id}`} type="button" className="btn-info-modal" onClick={toggle} />
+      <Popover
+        isOpen={isOpen}
+        trigger="legacy"
+        placement="auto"
+        toggle={toggle}
+        target={`button-to-activate-popup-${id}`}
+      >
+        <PopoverHeader>
+          <span>{textObj.header}</span>
+          <button className="kth-icon-button close" onClick={toggle} aria-label="Close" />
+        </PopoverHeader>
+
+        <PopoverBody>
+          <p>{textObj.body}</p>
+        </PopoverBody>
+      </Popover>
+    </>
+  )
+}
 
 export default InfoButton
